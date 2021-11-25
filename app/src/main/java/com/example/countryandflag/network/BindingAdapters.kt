ca2497cc8667323653690464a1e456ae73ae4b8a
khalid <kaled1418g@gmai.com>
2021-11-25 11:07:08 +0300

    Update to fix some bugs

--- app/src/main/java/com/example/countryandflag/network/BindingAdapters.kt
+++ app/src/main/java/com/example/countryandflag/network/BindingAdapters.kt
@@ -5,22 +5,39 @@ import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
 import coil.load
+import coil.request.ImageRequest
 import com.example.countryandflag.ImageAdapter
 import com.example.countryandflag.R
 
 @BindingAdapter("flag")
-fun bindImage(imgView: ImageView , imgUrl:String){
-imgUrl.let {
-    val imgUri = imgUrl.toUri().buildUpon()?.scheme("https")?.build()
-    imgView.load(imgUri){
-        placeholder(R.drawable.loading_animation)
-        error(R.drawable.ic_broken_image)
+fun bindImage(imgView: ImageView, imgUrl: String) {
+    imgUrl.let {
+          val imageLoader = ImageLoader.Builder(imgView.context)
+        .componentRegistry { add(SvgDecoder(imgView.context)) }
+        .build()
+
+
+
+        val request = ImageRequest.Builder(imgView.context)
+            .crossfade(true)
+            .crossfade(500)
+            .data(imgUrl)
+            .target(imgView)
+            .build()
+
+        imageLoader.enqueue(request)
+
+
+
     }
 }
-}
+
+
 @BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView , data:List<FlagPhoto>?){
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<FlagPhoto>?) {
     val adapter = recyclerView.adapter as ImageAdapter
     adapter.submitList(data)
 }
